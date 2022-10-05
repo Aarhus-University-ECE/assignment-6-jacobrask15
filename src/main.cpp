82ee7922e03f6cae0dc34cf66efefe502a16d7ba
jacobrask15 <49784405+jacobrask15@users.noreply.github.com>
2022-10-05 09:30:43 +0200

    Solution added

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 
 // File for sandboxing and trying out code
-int main(int argc, char **argv)
+//int main(int argc, char **argv)
+//{
+//    return 0;
+//}
+
+#include <stdio.h>
+#include <assert.h>
+
+int max(int* numbers, int size)
 {
-    return 0;
+assert(size>0);
+
+int maxvalue = numbers[0]; /* Variable to store max value set to first place in array */
+
+   /* for loop to search through array to find max value */
+   for (int i = 1; i < size; i++) {
+        if (numbers[i] > maxvalue)
+            maxvalue = numbers[i];
+   }
+
+    return maxvalue;
+}
+
+int main(){
+
+int numbers1[] = {5, 10, 20, 6, 10, -1, 9};
+
+printf("HØjeste tal i array: %d", max(numbers1, 2));
+
+return 0;
 }
--- src/max.c
+++ src/max.c
@@ -3,8 +3,15 @@
 
 int max(int* numbers, int size)
 {
-    // Excercise 2
-    // Implement your code below...
+assert(size>0);
 
-    return -1;
+int maxvalue = numbers[0]; /* Variable to store max value set to first place in array */
+
+   /* for loop to search through array to find max value */
+   for (int i = 1; i < size; i++) {
+        if (numbers[i] > maxvalue)
+            maxvalue = numbers[i];
+   }
+
+    return maxvalue;
 }
